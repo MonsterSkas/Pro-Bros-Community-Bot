1d0ef509023612b0044ab753c684f8d7a1150cfe
MonsterSkas <skyperflames@gmail.com>
2022-12-23 12:36:06 +0530

    edit main

--- main.py
+++ main.py
@@ -78,7 +78,6 @@ while True:
     try:
         client.run(os.environ["TOKEN"])
     except nextcord.errors.HTTPException as err:
-        status = err.status_code
-        if status == 429:
+        if err.status == 429:
             print("\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
             os.system("kill 1")
